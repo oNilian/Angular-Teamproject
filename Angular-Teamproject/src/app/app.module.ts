8e2483202ea483b2d9a6b6ca8c6117c55e6c1faf
oNilian <julian@alsen.se>
2019-04-02 11:45:52 +0200

    Navi Module created

--- Angular-Teamproject/src/app/app.module.ts
+++ Angular-Teamproject/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationModule } from './navigation/navigation.module';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    NavigationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
