7873be9d7425d7ebb82e282e5c5c40f14ca0bd4a
IbraCadaBra010101 <ibma010101@gmail.com>
2019-04-02 12:46:10 +0200

    ratinglist module adding

--- Angular-Teamproject/src/app/app.module.ts
+++ Angular-Teamproject/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {RatinglistModule} from './ratinglist/ratinglist.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RatinglistModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
