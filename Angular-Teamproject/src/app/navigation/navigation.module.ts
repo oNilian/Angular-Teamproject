867539ae310f4538959144e25778b2b321803cc5
oNilian <julian@alsen.se>
2019-04-02 14:07:02 +0200

    All componenter created and connected

--- Angular-Teamproject/src/app/navigation/navigation.module.ts
+++ Angular-Teamproject/src/app/navigation/navigation.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   exports: [
     CommonModule,
-    LandingPageComponent
-    
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class NavigationModule { }
