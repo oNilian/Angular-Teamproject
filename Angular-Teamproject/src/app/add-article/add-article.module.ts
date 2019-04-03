d8dbcb8d54ba52c12a916e60ac6add343b88f886
oNilian <julian@alsen.se>
2019-04-03 16:08:19 +0200

    added new module article

---
+++ Angular-Teamproject/src/app/add-article/add-article.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddNewArticleComponent } from './add-new-article/add-new-article.component';
+
+@NgModule({
+  declarations: [AddNewArticleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AddArticleModule { }
