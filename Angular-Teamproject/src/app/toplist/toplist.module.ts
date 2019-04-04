175e1fa3fbfb78acd594a53e10f8ed88f29b2b9d
Viktor <43089746+Zadazi@users.noreply.github.com>
2019-04-04 16:12:24 +0200

    created top 10 component

--- Angular-Teamproject/src/app/toplist/toplist.module.ts
+++ Angular-Teamproject/src/app/toplist/toplist.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopTenComponent } from './top-ten/top-ten.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TopTenComponent],
   imports: [
     CommonModule
   ]
