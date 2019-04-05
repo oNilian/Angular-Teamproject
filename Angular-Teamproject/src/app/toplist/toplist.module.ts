668370c34337d6ac5e886de45e56d89bb8bee4f1
Viktor <43089746+Zadazi@users.noreply.github.com>
2019-04-05 07:07:46 +0200

    added forgoten export

--- Angular-Teamproject/src/app/toplist/toplist.module.ts
+++ Angular-Teamproject/src/app/toplist/toplist.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { TopTenComponent } from './top-ten/top-ten.component';
 
 @NgModule({
-  declarations: [
-    TopTenComponent
-  ],
+  declarations: [TopTenComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [TopTenComponent]
 })
 export class ToplistModule { }
