d7f7bdcc2f1f8eb6fde1adbb4dd94eb26e77c90c
Viktor <43089746+Zadazi@users.noreply.github.com>
2019-04-05 07:57:08 +0200

    Added top ten subscription and move top ten sorting from service to responsible component

--- Angular-Teamproject/src/app/shared/database-api.service.ts
+++ Angular-Teamproject/src/app/shared/database-api.service.ts
@@ -12,21 +12,16 @@ export class DatabaseApiService {
   private groupID = '&group=7ZWHL'; // Used for test create new for final.
 
   getData(): Observable<any> {
+    console.log('getData');
     return this.http.get(this.baseURL + 'whathaveidone' + this.groupID);
   }
   setData(key: object, value: number = 0): Observable<any> {
-    console.log(key);
+    console.log('setData');
     return this.http.get(this.baseURL + 'op=set' + this.groupID + '&key=' + key + '&value=' + value);
   }
   delData(key: object) {
-    console.log(key);
+    console.log('delData');
     return this.http.get(this.baseURL + 'op=remove' + this.groupID + '&key=' + key);
-  }
-  getTop10() {
-    const objectDatabse: object = this.getData();
-    // let topTenList = objectDatabse.data.sort( (x: object, y: object) => y.value - x.value );
-    // topTenList = topTenList.slice(0, 10);
-    // return topTenList;
   }
 
   constructor(private http: HttpClient) { }
--- Angular-Teamproject/src/app/toplist/top-ten/top-ten.component.ts
+++ Angular-Teamproject/src/app/toplist/top-ten/top-ten.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DatabaseApiService } from '../../shared/database-api.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-top-ten',
   templateUrl: './top-ten.component.html',
   styleUrls: ['./top-ten.component.css']
 })
 export class TopTenComponent implements OnInit {
+  public getDatabase: Observable<any>;
 
-  constructor() { }
+  constructor(private database: DatabaseApiService) { }
 
   ngOnInit() {
+    this.database.getData().subscribe(dataHandler => {
+      console.log(dataHandler);
+      if (dataHandler.status === 'success') {
+        console.log('Top-ten subscribtion got a good response');
+        // map data
+        let topTenList = dataHandler.data.sort( (x: any, y: any) => y.value - x.value );
+        topTenList = topTenList.slice(0, 10);
+        console.log(topTenList);
+      } else {
+        console.log('Top-ten subscribtion error!');
+      }
+    });
   }
 
 }
