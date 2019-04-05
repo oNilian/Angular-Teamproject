1e0bc716afdbfb903f5b5fe5fa0daafe1292e2ae
Viktor <43089746+Zadazi@users.noreply.github.com>
2019-04-05 09:51:56 +0200

    added better sample data

--- Angular-Teamproject/src/app/shared/database-api.service.ts
+++ Angular-Teamproject/src/app/shared/database-api.service.ts
@@ -12,19 +12,19 @@ export class DatabaseApiService {
   private groupID = '&group=YNeBZ'; // Used for test create new for final.
 /*
 Since key is used for delete it we cannot store ranking data there so maybe change to store it in the value object {..., rating: 0 }
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 1', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 2', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 3', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 4', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 5', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 6', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 7', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 8', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 9', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 10', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 11', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 12', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
-https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4&value={title: 'Random wiki entry 13', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random'}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4123&value={title: 'Random wiki entry 1', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 5}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4523&value={title: 'Random wiki entry 2', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 0}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4534&value={title: 'Random wiki entry 3', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 1}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4143&value={title: 'Random wiki entry 4', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 3}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4123&value={title: 'Random wiki entry 5', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 2}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=4324&value={title: 'Random wiki entry 6', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 3}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=6344&value={title: 'Random wiki entry 7', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 4}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=8534&value={title: 'Random wiki entry 8', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 1}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=9854&value={title: 'Random wiki entry 9', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 5}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=6424&value={title: 'Random wiki entry 10', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 2}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=6454&value={title: 'Random wiki entry 11', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 0}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=9454&value={title: 'Random wiki entry 12', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 3}
+https://forverkliga.se/JavaScript/api/api-db.php?op=set&group=YNeBZ&key=7534&value={title: 'Random wiki entry 13', article: 'long information about the entry', link: 'http://en.wikipedia.org/wiki/random', rating: 5}
 */
   getData(): Observable<any> {
     console.log('getData');
