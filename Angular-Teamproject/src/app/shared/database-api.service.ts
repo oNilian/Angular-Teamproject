394b8db3d3633971dcd0e89d7476b14876ab96b2
Viktor <43089746+Zadazi@users.noreply.github.com>
2019-04-08 13:44:54 +0200

    Fixed problem with `"`

--- Angular-Teamproject/src/app/shared/database-api.service.ts
+++ Angular-Teamproject/src/app/shared/database-api.service.ts
@@ -9,17 +9,20 @@ export class DatabaseApiService {
 
   private baseURL: string = 'https://forverkliga.se/JavaScript/api/api-db.php?';
   private groupID: string = '&group=1av0E'; // Used for test create new for final.
-  private dataArray = [];
   
   getData(): Observable<any> {
     return this.http.get(this.baseURL + 'whathaveidone' + this.groupID);
   }
   
-  setData(title: string, article: string, url: string, rating: number = 0): Observable<any> {
+  setData(title: string, article: string, url: string, rating = 0): Observable<any> {
     const uniqueID =  Math.floor((Math.random() * 999999) + 1);
-    const valueString = `{"title": "${title}", "article": "${article}", "link": "${url}", "rating": ${rating}}`;
-
-    this.http.get(this.baseURL + 'op=set' + this.groupID + '&key=' + uniqueID + '&value=' + valueString).subscribe();
+    const value = encodeURI(JSON.stringify({
+      title: title,
+      article: article,
+      link: url,
+      rating: rating
+    }));
+    this.http.get(this.baseURL + 'op=set' + this.groupID + '&key=' + uniqueID + '&value=' + value).subscribe();
     return;
   }
 
